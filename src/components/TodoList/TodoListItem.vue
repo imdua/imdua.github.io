22627685291e44f2020136cc28e483cbae4a129c
Yoon Yeom <yoonyeom@seowoninfo.com>
2024-01-30 17:37:02 +0900

    업로드

---
+++ src/components/TodoList/TodoListItem.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { ref } from "vue";
+import "./TodoListItem.scss";
+
+/**
+ * defineProps와 defineEmits는 script setup 내에서만 사용할 수 있으며,
+ * import 할 필요 없이 script setup이 처리될 때 컴파일 된다.
+ */
+const props = defineProps({
+  todo: {
+    type: Object,
+    default: () => {},
+  },
+});
+const text = ref(props.todo.text);
+const onToggle = (id) => {
+  console.log(id);
+};
+</script>
+
+<template>
+  <div class="TodoListItem-virtualized">
+    <li class="TodoListItem">
+      <div
+        :class="['checkbox', { checked: todo.checked }]"
+        @click="onToggle(todo.id)"
+      >
+        <input type="checkbox" :checked="todo.checked" />
+        <div class="text">{{ text }}</div>
+      </div>
+      <div
+        class="edit"
+        @click="
+          () => {
+            onChangeSelectedTodo(todo);
+            onInsertToggle();
+          }
+        "
+      >
+        <font-awesome-icon :icon="['fas', 'pen']" />
+      </div>
+      <div class="remove" @click="() => onRemove(id)">
+        <font-awesome-icon :icon="['fas', 'circle-minus']" />
+      </div>
+    </li>
+  </div>
+</template>
